58850d23d6e634bc6d3e8c7aba3a892f9dfca225
Laika-Beats <laikabeatsdenver@gmail.com>
2020-12-03 11:16:01 -0700

    server routes, cors, bodyparser

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
+const bodyParser = require("body-parser")
 const app = express();
 const PORT = process.env.PORT || 3001;
+const authRoutes = require("./routes/auth")
+const { db } = require("./models/User")
+require("dotenv").config;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
+app.use("/api", authRoutes)
+
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/secret-santa");
